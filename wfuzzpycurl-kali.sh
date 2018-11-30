908762193c8ace359b64065ee52e8de4a1726678
Volken <the.shadow.anonyme@gmail.com>
2018-11-30 17:23:01 +0100

    Add files via upload

---
+++ wfuzzpycurl-kali.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+#
+#   How to rebuild PycURL against OpenSSL on Kali Rolling
+#   (tested for Kali Rolling 2018.4)
+#   Edit https://github.com/adon90/openssl_wfuzz/wfuuzpycurl.sh specially for Kali
+#   By Volken
+#
+
+
+ESC="\x1b["
+RESET=$ESC"39;49;00m"
+RED=$ESC"31;01m"
+GREEN=$ESC"32;01m"
+YELLOW=$ESC"33;01m"
+BLUE=$ESC"34;01m"
+
+header() {
+    echo -e "\n$YELLOW --- $1 --- $RESET\n"
+}
+
+echo -e "\n\n$GREEN"
+echo -e "\
+Build pycurl with openssl support\n\n\
+Section numbers refer to\n\
+https://gist.github.com/aerickson/f15133a7e56b2d7f27e3
+"
+echo -e "$RESET\n"
+
+if grep -Fxq "deb-src http://http.kali.org/kali kali-rolling main non-free contrib" /etc/apt/sources.list
+then echo ""
+else echo "deb-src http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list 
+fi
+
+sudo apt-get update
+
+cd /tmp
+
+# remove eventually existing pycurl
+header "0. remove python-pycurl"
+sudo apt-get remove python-pycurl --yes
+
+header "1. install build essentials"
+sudo apt-get install build-essential fakeroot dpkg-dev --yes
+
+header "2./3. make build dir"
+mkdir /tmp/python-pycurl-openssl
+cd /tmp/python-pycurl-openssl
+
+if [ ! "/tmp/python-pycurl-openssl" = `pwd` ]; then
+    echo -e "\n${RED}Cannot change to working dir. WTF?${RESET}\n"
+    exit 1
+fi
+
+header "4. get pycurl sources"
+sudo apt-get source python-pycurl --yes
+
+header "5. get build dependencies for python-pycurl"
+sudo apt-get build-dep python-pycurl --yes
+
+header "6. install libcurl with openssl"
+sudo apt-get install libcurl4-openssl-dev libssl-dev --yes
+
+DIR=`find * -name 'pycurl*' -type d -print`
+DSC=`find * -name '*.dsc' -type f -print`
+
+echo
+echo "DIR = $DIR"
+echo "DSC = $DSC"
+echo
+
+header "7. delete previous dir"
+sudo rm -R pycurl-7.43.0.1
+
+header "8. unpack source archive"
+sudo dpkg-source -x $DSC
+
+header "9. change to package dir"
+cd $DIR
+
+header "10. edit debian/control file"
+sudo cp debian/control ./control.gnutls
+sudo cat ./control.gnutls | sed -e 's|libcurl4-gnutls-dev|libcurl4-openssl-dev|' | sudo tee ./control.openssl >/dev/null
+sudo cp ./control.openssl debian/control
+
+header "11. build package"
+echo "hold on..."
+echo "(crashdump of nosetests is 'normal')"
+sudo PYCURL_SSL_LIBRARY=openssl dpkg-buildpackage -rfakeroot -b 2>&1 | sudo tee ../buildlog.txt >/dev/null
+echo
+echo -e $BLUE
+sudo head ../buildlog.txt
+echo -e $RESET
+echo "   [ ... 1000+ more messages omitted ... ]"
+echo -e $BLUE
+sudo tail ../buildlog.txt
+echo -e $RESET
+echo
+echo "find more output in ~/python-pycurl-openssl/buildlog.txt:"
+wc -l  ../buildlog.txt
+
+header "12. install via package manager"
+sudo dpkg -i ../python-pycurl_*.deb
+
+header "13. make the change permanent"
+sudo apt-mark hold python-pycurl
+
+header "14. reinstalling wfuzz with openssl"
+apt-get install wfuzz
+
+echo
+echo "some other packages are available also now:"
+ls ../*.deb
+echo "they can be installed by 'sudo dpkg -i ../py...' from here."
+
+echo "Enjoy with wfuzz! -Volken"
